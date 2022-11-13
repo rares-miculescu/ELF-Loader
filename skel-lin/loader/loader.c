0a8634dc3560d66b526726299c937444640c8759
rares-miculescu <miculescurares@yahoo.com>
2022-11-13 10:31:50 -0500

    first commit

---
+++ skel-lin/loader/loader.c
@@ -0,0 +1,45 @@
+/*
+ * Loader Implementation
+ *
+ * 2022, Operating Systems
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "exec_parser.h"
+
+static so_exec_t *exec;
+
+static void segv_handler(int signum, siginfo_t *info, void *context)
+{
+	/* TODO - actual loader implementation */
+}
+
+int so_init_loader(void)
+{
+	int rc;
+	struct sigaction sa;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_sigaction = segv_handler;
+	sa.sa_flags = SA_SIGINFO;
+	rc = sigaction(SIGSEGV, &sa, NULL);
+	if (rc < 0) {
+		perror("sigaction");
+		return -1;
+	}
+	return 0;
+}
+
+int so_execute(char *path, char *argv[])
+{
+	exec = so_parse_exec(path);
+	if (!exec)
+		return -1;
+
+	so_start_exec(exec, argv);
+
+	return -1;
+}
