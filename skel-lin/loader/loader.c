cb3609fe0d47d4fe4671a65c1688ddba0e01fcf2
rares-miculescu <miculescurares@yahoo.com>
2022-11-21 23:43:27 +0200

    page offset si mmap

--- skel-lin/loader/loader.c
+++ skel-lin/loader/loader.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/mman.h>
+
 
 #include "exec_parser.h"
 
@@ -18,27 +20,58 @@ static so_exec_t *exec;
 
 so_seg_t* findSegment(void *addr){
 
-
+	printf("%d\n", (*exec).segments_no);
 	for(int i = 0; i < (*exec).segments_no; i++){
 
-		if((char *)exec->segments[i].vaddr > (char *)addr){
-			return &(exec->segments[i - 1]);
+		printf("%d ",i);
+		if((char *)exec->segments[i].vaddr < (char *)addr
+		 && (char *)addr < ((char *)exec->segments[i].vaddr + exec->segments[i].mem_size)){
+			printf("intrat\n");
+			return &(exec->segments[i]);
 				
 		}
 	}
+	printf("\n");
 
 	return null;
 
 }
 
 
+
+
 static void segv_handler(int signum, siginfo_t *info, void *context)
 {
 	/* TODO - actual loader implementation */
-	size_t pgsize = getpagesize();
 	so_seg_t *sgm = findSegment(info->si_addr);
+
+	if(sgm == null){
+		// struct sigacion sa;
+		// memset(&sa, 0, sizeof(sa));
+		// sigaction(SIGSEGV, &sa, 0);
+
+		exit(SIGSEGV_ERROR);
+
+	}
 	
+
+	printf("imi da si return de: %p\n", sgm->vaddr);
 	
+	size_t pgsize = getpagesize();
+	size_t seg_offset = (char *)info->si_addr - (char *)sgm->vaddr;
+	size_t pg_offset = seg_offset % pgsize;
+	seg_offset -= pg_offset;
+
+	printf("seg_offset = %ld\n", (long)seg_offset);
+
+
+
+
+	mmap(sgm->vaddr + seg_offset, pgsize, PROT_READ | PROT_WRITE, MAP_FIXED, 0, 0);
+
+
+
+
 }
 
 int so_init_loader(void)
