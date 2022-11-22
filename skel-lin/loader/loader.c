39a997397ad5d1ca592f16223c34c986a23efaf4
rares-miculescu <miculescurares@yahoo.com>
2022-11-22 19:22:44 +0200

    ruleaza 80/95

--- skel-lin/loader/loader.c
+++ skel-lin/loader/loader.c
@@ -17,7 +17,7 @@
 
 #define null NULL
 
-uintptr_t *valid;
+int *valid;
 
 static so_exec_t *exec;
 
@@ -26,9 +26,12 @@ int fd;
 so_seg_t* findSegment(void *addr){
 
 	// printf("segments_no: %d\n", (*exec).segments_no);
+	// printf("imi da fault pe adresa %p\n", addr);
 	for(int i = 0; i < (*exec).segments_no; i++){
 
-		if((char *)exec->segments[i].vaddr < (char *)addr
+		// printf("segaddr[%d] = %p\n", i, (void *)exec->segments[i].vaddr);
+
+		if((char *)exec->segments[i].vaddr <= (char *)addr
 		 && (char *)addr < ((char *)exec->segments[i].vaddr + exec->segments[i].mem_size)){
 			return &(exec->segments[i]);
 				
@@ -40,29 +43,34 @@ so_seg_t* findSegment(void *addr){
 
 }
 
-void validate(uintptr_t pg_addr){
-
-	size_t addr_size = sizeof(pg_addr);
-	printf("addr_size: %ld\n", (long)addr_size);
+void validate(void *pg_addr, size_t pgsize){
 
 	if(valid == null){
-		printf("imi adauga prima adresa: %p\n", (void *)pg_addr);
-		valid = malloc(sizeof(pg_addr));
-		(*valid) = pg_addr;
+
+		// printf("imi adauga prima adresa: %p\n", (void *)pg_addr);
+		
+		valid = malloc(sizeof(int));
+		(*valid) = (int)pg_addr;
 		return;
 	}
 	
-	uintptr_t *prc = valid;
-	for(; (void *)(*prc) != null; prc += addr_size){
-		if((*prc) == pg_addr){
-			printf("am gasit segmentul deja: %p\n", (void *)pg_addr);
+	int *prc = valid;
+	for(int i = 0; i < sizeof(valid) / sizeof(int *); i += sizeof(int)){
+
+		// printf("adresa deja mapata %d\n", (*prc));
+
+		if(*(prc + i) == (int)pg_addr){
+
+			// printf("am gasit segmentul deja: %p\n", (void *)pg_addr);
+			
 			exit(139);
 		}
 	}
 
-	printf("nu e primul element si imi adauga adresa: %p\n", (void *)pg_addr);
-	uintptr_t *aux = realloc(valid, sizeof(valid) + addr_size);
-	*(aux + (sizeof(valid))) = pg_addr;
+	// printf("nu e primul element si imi adauga adresa: %p\n", (void *)pg_addr);
+
+	int *aux = realloc(valid, sizeof(valid) + sizeof(int));
+	*(aux + (sizeof(valid))) = (int)pg_addr;
 	// free(valid);
 	valid = aux;
 
@@ -77,18 +85,23 @@ void cpy_mem(void *pg_addr, int seg_offset, so_seg_t *sgm, size_t page_size){
 	
 	memset(buffer, 0, page_size);
 	
+	// printf("filesize %u si pagesize %ld si memsize %u\n", sgm->file_size, (long)page_size, sgm->mem_size);
+
 	size_t size = seg_offset + page_size;
 	if(seg_offset > sgm->file_size){
+		// printf("e dupa filesize\n");
 		memcpy((void *)pg_addr, buffer, page_size);
 		// free(buffer);
 		return;
 	}
 	if(size > sgm->file_size){
+		// printf("e putin in filesize\n");
 		read(fd, buffer, sgm->file_size - seg_offset);
 		memcpy((void *)pg_addr, buffer, page_size);
 		// free(buffer);
 		return;
 	}
+	// printf("e in filesize");
 	read(fd, buffer, page_size);
 	memcpy((void *)pg_addr, buffer, page_size);
 	// free(buffer);
@@ -110,23 +123,27 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 
 	}
 
-	printf("imi da si return de: %p\n", (void *)sgm->vaddr);
+	// printf("imi da si return de: %p\n", (void *)sgm->vaddr);
 	
 	size_t pgsize = getpagesize();
 	size_t seg_offset = (char *)info->si_addr - (char *)sgm->vaddr;
 	size_t pg_offset = seg_offset % pgsize;
 	seg_offset -= pg_offset;
 
-	printf("seg_offset = %ld\n", (long)seg_offset);
+	// printf("seg_offset = %ld\n", (long)seg_offset);
 
-	validate(sgm->vaddr + seg_offset);
+	validate((void *)sgm->vaddr + seg_offset, pgsize);
 
-	void *pgaddr = mmap((void *)(sgm->vaddr + seg_offset), 
-	pgsize, PROT_READ | PROT_WRITE, MAP_FIXED, 0, 0);
+	void *pgaddr = mmap((void *)(sgm->vaddr) + seg_offset, 
+	pgsize, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS, 0, 0);
 
-	printf("a facut mmap la %p", pgaddr);
+	// printf("a facut mmap la %p\n", pgaddr);
 
 	cpy_mem(pgaddr, seg_offset, sgm, pgsize);
+
+	mprotect(pgaddr, pgsize, sgm->perm);
+
+	// printf("\n\n");
 
 }
 
