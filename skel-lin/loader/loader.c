7f288207638581811ad2f436603fe9d0c27172a5
rares-miculescu <miculescurares@yahoo.com>
2022-11-24 12:31:12 +0200

    finished

--- skel-lin/loader/loader.c
+++ skel-lin/loader/loader.c
@@ -17,20 +17,20 @@
 
 #define null NULL
 
+//array where we store what pages were added
 long *valid;
 
+//struct of ELF executable
 static so_exec_t *exec;
 
+//file descriptor
 int fd;
 
-so_seg_t* findSegment(void *addr){
 
-	// printf("(find)segments_no: %d | ", (*exec).segments_no);
-	// printf("(find)imi da fault pe adresa %p | ", addr);
+//function that finds the segment that contains the given address
+so_seg_t* findSegment(void *addr){
 	
 	for(int i = 0; i < (*exec).segments_no; i++){
-
-		// printf("segaddr[%d] = %p | ", i, (void *)exec->segments[i].vaddr);
 
 		if((char *)exec->segments[i].vaddr <= (char *)addr
 		 && (char *)addr < ((char *)exec->segments[i].vaddr + exec->segments[i].mem_size)){
@@ -38,17 +38,16 @@ so_seg_t* findSegment(void *addr){
 				
 		}
 	}
-	// printf("\n");
 
 	return null;
 
 }
 
+//function that uses the array valid to verifiy if the address was already mapped or not
+//if it was mapped exits with seg fault, else it adds the address to the array
 void validate(void *pg_addr, size_t pgsize){
 
 	if(valid == null){
-
-		// printf("(val)imi adauga prima adresa: %p | ", (void *)pg_addr);
 		
 		valid = malloc(sizeof(long));
 		(*valid) = (long)pg_addr;
@@ -58,25 +57,21 @@ void validate(void *pg_addr, size_t pgsize){
 	long *prc = valid;
 	for(int i = 0; i < sizeof(valid) / sizeof(long *); i += sizeof(long)){
 
-		// printf("(val)adresa deja mapata %lx | ", (*prc));
-
 		if(*(prc + i) == (long)pg_addr){
-
-			// printf("(val)am gasit segmentul deja: %p | ", (void *)pg_addr);
 			
 			exit(139);
 		}
 	}
 
-	// printf("(val)nu e primul element si imi adauga adresa: %p | ", (void *)pg_addr);
-
 	long *aux = realloc(valid, sizeof(valid) + sizeof(long));
 	*(aux + (sizeof(valid))) = (long)pg_addr;
 	valid = aux;
 
-
 }
 
+//copies the memory on the virtual memory
+//we have 3 cases here, where the entire page is in the filesize, when a part of 
+//the page is in the filesize and when none of the page is in the filesize
 void cpy_mem(void *pg_addr, int seg_offset, so_seg_t *sgm, size_t page_size){
 
 	char *buffer = malloc(page_size);
@@ -84,35 +79,34 @@ void cpy_mem(void *pg_addr, int seg_offset, so_seg_t *sgm, size_t page_size){
 	lseek(fd, sgm->offset + seg_offset, SEEK_SET);
 	
 	memset(buffer, 0, page_size);
-	
-	// printf("(cpm)filesize %u si pagesize %ld si memsize %u | ", sgm->file_size, (long)page_size, sgm->mem_size);
 
 	size_t size = seg_offset + page_size;
-	// printf("(cpm)pgaddr = %p | ", pg_addr);
+
 	if(seg_offset > sgm->file_size){
-		// printf("(cpm)e dupa filesize | ");
+
 		memcpy((void *)pg_addr, buffer, page_size);
 		free(buffer);
 		return;
 	}
 	if(size > sgm->file_size){
-		// printf("(cpm)e putin in filesize | ");
+
 		read(fd, buffer, sgm->file_size - seg_offset);
 		memcpy((void *)pg_addr, buffer, page_size);
 		free(buffer);
 		return;
 	}
-	// printf("(cpm)e in filesize | ");
+
 	read(fd, buffer, page_size);
 	memcpy((void *)pg_addr, buffer, page_size);
 	free(buffer);
 
 }
 
-
+//the handler finds the segment where the fault was made, calculates the 
+//address of the page, chacks if it was already added, maps the page and
+//copies the page from the file to the memory
 static void segv_handler(int signum, siginfo_t *info, void *context)
 {
-	/* TODO - actual loader implementation */
 	so_seg_t *sgm = findSegment(info->si_addr);
 
 	if(sgm == null){
@@ -123,32 +117,20 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 		exit(139);
 
 	}
-
-	// printf("(main)imi da si return de: %p | ", (void *)sgm->vaddr);
 	
 	size_t pgsize = getpagesize();
 	size_t seg_offset = (char *)info->si_addr - (char *)sgm->vaddr;
 	size_t pg_offset = seg_offset % pgsize;
 	seg_offset -= pg_offset;
 
-	// printf("(main)seg_offset = %ld | ", (long)seg_offset);
-
 	validate((void *)sgm->vaddr + seg_offset, pgsize);
 
 	void *pgaddr = mmap((void *)(sgm->vaddr) + seg_offset, 
 	pgsize, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS, 0, 0);
 
-	// printf("(main)a facut mmap la %p | ", pgaddr);
-
-	// printf("nuj\n");
-
 	cpy_mem(pgaddr, seg_offset, sgm, pgsize);
 
-	// printf("nuj");
-
 	mprotect(pgaddr, pgsize, sgm->perm);
-
-	// printf("                                              ");
 
 }
 
